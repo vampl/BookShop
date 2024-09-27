5d63a6bac25e4a3661f716c1edd3b5da3181c1c3
Vitalii Barabash <vitalii_barabash@epam.com>
2024-09-27 20:49:20 +0300

    implemented database context with common data sets and configuration.

---
+++ BookShopApi/Data/BookShopDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BookShopApi.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopApi.Data;
+
+public class BookShopDbContext : DbContext
+{
+    public BookShopDbContext(DbContextOptions<BookShopDbContext> options) : base(options) { }
+
+    public DbSet<Book> Books { get; set; }
+
+    public DbSet<Genre> Genres { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(builder =>
+        {
+            builder.HasKey(book => book.Isbn);
+        });
+
+        modelBuilder.Entity<Genre>(builder =>
+        {
+            builder.HasKey(genre => genre.Id);
+
+            builder.HasMany<Book>(genre => genre.Books)
+                .WithOne(book => book.Genre)
+                .HasForeignKey(book => book.GenreId);
+        });
+    }
+}
