3d9bc37c08da3f1a7d181a4b172693b8c8073462
Vitalii Barabash <vitalii_barabash@epam.com>
2024-09-27 21:57:02 +0300

    implemented genres api controller request handling.

---
+++ BookShopApi/Controllers/GenreController.cs
@@ -0,0 +1,79 @@
+﻿using BookShopApi.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+using BookShopApi.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GenreController : ControllerBase
+{
+    private readonly BookShopDbContext _context;
+
+    public GenreController(BookShopDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Genre>> GetGenres()
+    {
+        return Ok(_context.Genres.ToList());
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Genre> GetGenre(int id)
+    {
+        Genre? genre = _context.Genres.Find(id);
+        if (genre is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(genre);
+    }
+
+    [HttpPost]
+    public ActionResult<Genre> PostGenre([FromBody] Genre genre)
+    {
+        _context.Genres.Add(genre);
+        _context.SaveChanges();
+
+        return CreatedAtAction("PostGenre", genre);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult PutGenre(int id, [FromBody] Genre modifiedGenre)
+    {
+        Genre? genre = _context.Genres.Find(id);
+        if (genre is null)
+        {
+            return NotFound();
+        }
+
+        _context.Entry(genre).CurrentValues.SetValues(modifiedGenre);
+        _context.Entry(genre).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteGenre(int id)
+    {
+        Genre? genre = _context.Genres.Find(id);
+        if (genre is null)
+        {
+            return NotFound();
+        }
+
+        _context.Genres.Remove(genre);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+}
