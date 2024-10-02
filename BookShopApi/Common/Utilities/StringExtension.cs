5005a13304879ffbf0091f05ddfa37c9ea51edbc
Vitalii Barabash <vitalii_barabash@epam.com>
2024-10-02 16:49:42 +0300

    implemented string extension to satisfy validation needs.

---
+++ BookShopApi/Common/Utilities/StringExtension.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShopApi.Common.Utilities;
+
+public static class StringExtensions
+{
+    public static int[] ExtractDigitsToArray(this string str)
+    {
+        ValidateInput(str);
+
+        var digits = new int[str.Length];
+        for (var i = 0; i < str.Length; i++)
+        {
+            digits[i] = int.Parse(str[i].ToString());
+        }
+
+        return digits;
+    }
+
+    public static bool IsNumber(this string str)
+    {
+        return int.TryParse(str, out int _);
+    }
+
+    private static void ValidateInput(string str)
+    {
+        if (str == null)
+        {
+            throw new ArgumentNullException(nameof(str));
+        }
+
+        if (str == string.Empty)
+        {
+            throw new ArgumentException(string.Empty, nameof(str));
+        }
+
+        if (!str.IsNumber())
+        {
+            throw new ArgumentException(string.Empty, nameof(str));
+        }
+    }
+}
